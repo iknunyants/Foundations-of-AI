a02dea5ea435833ea912c195486813ed660bb99b
Ivan Knunyants <iknunyants@gmail.com>
2022-11-29 17:49:24 +0100

    code for assignment 1

--- team30_A1/sudokuai.py
+++ team30_A1/sudokuai.py
@@ -2,10 +2,100 @@
 #  Software License, (See accompanying file LICENSE or copy at
 #  https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import random
-import time
+import copy
 from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
 import competitive_sudoku.sudokuai
+
+
+def check_finish(board):
+    m = board.m
+    n = board.n
+    for i in range(m * n):
+        for j in range(m * n):
+            if board.get(i, j) == 0:
+                return False
+    return True
+
+
+def calculate_score(i, j, board):
+    score = [0, 1, 3, 7]
+    m = board.m
+    n = board.n
+    count = 0
+    flag = True
+    for x in range((i // m) * m, (i // m) * m + m):
+        for y in range((j // n) * n, (j // n) * n + n):
+            if board.get(x, y) == 0:
+                flag = False
+                break
+        if flag == False:
+            break
+    if flag:
+        count += 1
+    flag = True
+    for x in range(m * n):
+        if board.get(x, j) == 0:
+            flag = False
+            break
+    if flag:
+        count += 1
+    flag = True
+    for x in range(m * n):
+        if board.get(i, x) == 0:
+            flag = False
+            break
+    if flag:
+        count += 1
+    return score[count]
+
+
+def check_valid(i, j, value, board):
+    m = board.m
+    n = board.n
+
+    if board.get(i, j) != 0:
+        return False
+    for x in range(m * n):
+        if board.get(x, j) == value:
+            return False
+    for x in range(m * n):
+        if board.get(i, x) == value:
+            return False
+    for x in range((i // m) * m, (i // m) * m + m):
+        for y in range((j // n) * n, (j // n) * n + n):
+            if board.get(x, y) == value:
+                return False
+    return True
+
+
+def minimax(depth, board, is_max, taboo, score1=0, score2=0):
+    if check_finish(board) or depth == 0:
+        return score1 - score2
+    available_list = []
+    for i in range(board.m * board.n):
+        for j in range(board.m * board.n):
+            for v in range(1, board.m * board.n + 1):
+                if check_valid(i, j, v, board):
+                    available_list.append((i, j, v))
+    available_list = list(set(available_list) - set(taboo))
+    if is_max:
+        value = -100000
+        for i in available_list:
+            newboard = copy.deepcopy(board)
+            newboard.put(i[0], i[1], i[2])
+            score = calculate_score(i[0], i[1], newboard)
+            value = max(value, minimax(depth - 1, newboard,
+                        False, taboo, score1 + score, score2))
+        return value
+    else:
+        value = 100000
+        for i in available_list:
+            newboard = copy.deepcopy(board)
+            newboard.put(i[0], i[1], i[2])
+            score = calculate_score(i[0], i[1], newboard)
+            value = min(value, minimax(depth - 1, newboard,
+                        True, taboo, score1, score2 + score))
+        return value
 
 
 class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
@@ -15,20 +105,31 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
 
     def __init__(self):
         super().__init__()
+        self.solve_sudoku_path = None  # N.B. this path is set from outside
 
-    # N.B. This is a very naive implementation.
+    # Uses solve_sudoku to compute a random move.
     def compute_best_move(self, game_state: GameState) -> None:
-        N = game_state.board.N
-
-        def possible(i, j, value):
-            return game_state.board.get(i, j) == SudokuBoard.empty \
-                   and not TabooMove(i, j, value) in game_state.taboo_moves
-
-        all_moves = [Move(i, j, value) for i in range(N) for j in range(N)
-                     for value in range(1, N+1) if possible(i, j, value)]
-        move = random.choice(all_moves)
-        self.propose_move(move)
-        while True:
-            time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
-
+        board = copy.copy(game_state.board)
+        satisfy = []
+        for i in range(board.m * board.n):
+            for j in range(board.m * board.n):
+                for v in range(1, board.m * board.n + 1):
+                    if check_valid(i, j, v, board):
+                        satisfy.append((i, j, v))
+        max_score = -1000
+        taboo = [(taboo_move.i, taboo_move.j, taboo_move.value)
+                 for taboo_move in game_state.taboo_moves]
+        for i in range(len(satisfy)):
+            if (satisfy[i][0], satisfy[i][1], satisfy[i][2]) in taboo:
+                continue
+            depth = 2
+            newboard = copy.deepcopy(board)
+            newboard.put(satisfy[i][0], satisfy[i][1], satisfy[i][2])
+            score = calculate_score(satisfy[i][0], satisfy[i][1], newboard)
+            current_score = minimax(
+                depth - 1, newboard, False, taboo, score, 0)
+            if max_score < current_score:
+                max_score = current_score
+                current_max = [satisfy[i][0], satisfy[i][1], satisfy[i][2]]
+                self.propose_move(
+                    Move(current_max[0], current_max[1], current_max[2]))
